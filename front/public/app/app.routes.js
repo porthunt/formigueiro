fc880b390157c6f3ba8ce8a00b00f083da675235
thomascruz <thomasagante@MacBook-Pro-de-Thomas.local>
2016-06-25 14:29:59 -0300

    Front first commit

---
+++ front/public/app/app.routes.js
@@ -0,0 +1,17 @@
+app.config(function($routeProvider, $locationProvider, $httpProvider) {
+
+	$routeProvider
+
+		.when('/', {
+			templateUrl: 'app/components/index/indexView.html',
+			controller: "IndexController",
+			replace: true,
+			reloadOnSearch: false
+		})
+		
+		// DEFAULT
+		.otherwise({
+			redirectTo: '/',
+		});
+
+});
---
+++ front/public/app/shared/header/headerDirective.js
@@ -0,0 +1,7 @@
+app.directive('headerElement', function() {
+	return {
+		restrict: 'AE',
+		replace: true,
+		templateUrl: 'app/shared/header/headerView.html'
+	}
+});
