970bdb6bfb725a7ed799067f29e6dcc75b95ea64
Joao Pedro Portela Gomez Vaz <jpvaz@br.ibm.com>
2016-06-25 16:10:05 -0300

    Company class
    Added Company class. The user is able to retrieve info of all companies
    and also from just one given an email.

---
+++ persistance/company/company.py
@@ -0,0 +1,42 @@
+import json
+import pycurl
+import StringIO
+
+from StringIO import StringIO
+
+'''
+Class Company
+
+add(name, city, state, cnpj, telephone, email) # add a company
+retrieve(email='') # retrieves all companies. If has argument email, returns just that company
+'''
+
+
+class Company(object):
+
+    def __init__(self, _conn):
+        self.conn = _conn
+
+    def add(self, _name, _city, _state, _cnpj, _telephone, _email):
+        pass
+
+    def retrieve(self, **kwargs):
+        response_buffer = StringIO()
+        curl = pycurl.Curl()
+        r_url = 'https://{}.cloudant.com/{}/_design/company/_view/'.format(self.conn.host, self.conn.db)
+
+        if 'email' in kwargs:
+            r_url += 'by_email?key="'+kwargs['email']+'"'
+        else:
+            r_url += 'all'
+
+        curl.setopt(curl.URL, r_url)
+        curl.setopt(curl.USERPWD, '%s:%s' % (self.conn.user, self.conn.pwd))
+        curl.setopt(curl.WRITEFUNCTION, response_buffer.write)
+        curl.perform()
+        curl.close()
+        resp_value = json.loads(response_buffer.getvalue())['rows']
+        return json.dumps(resp_value)
+
+    def update(self, **kwargs):
+        pass
--- server.py
+++ server.py
@@ -4,6 +4,7 @@ import os
 
 from flask import Flask
 from persistance.connect import Connection
+from persistance.company.company import Company
 
 app = Flask(__name__)
 app_name = 'FooBar'
@@ -18,7 +19,16 @@ def index():
     return 'Welcome to the {} API!'.format(app_name)
 
 
-@app.route('/api/test')
+@app.route('/api/company', methods=['GET'])
+@app.route('/api/company/<email>', methods=['GET'])
+def company(email=None):
+    if email is not None:
+        return Company(conn).retrieve(email='thom.agante@gmail.com')
+    else:
+        return Company(conn).retrieve()
+
+
+@app.route('/api/test-api')
 def test_connection():
     lst = list()
     for document in conn.test_connection():
@@ -27,7 +37,7 @@ def test_connection():
 
 
 @app.errorhandler(404)
-def page_not_found(e):
+def page_not_found():
     return 'api not found'
 
 
